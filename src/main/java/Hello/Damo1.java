05d4b1464ccdab92f34de9a617001c4de96113fe
Warre <2032820878@qq.com>
2022-01-03 17:37:22 +0800

    first commit

---
+++ src/main/java/Hello/Damo1.java
@@ -0,0 +1,7 @@
+package Hello;
+
+public class Damo1 {
+    public static void main(String[] args) {
+        System.out.println("牛皮");
+    }
+}
---
+++ src/main/java/Hello/Hello_Git.java
@@ -0,0 +1,8 @@
+package Hello;
+
+public class Hello_Git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
